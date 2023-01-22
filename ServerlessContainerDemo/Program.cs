b04c6583d4cfa29a0747b6ba3dec9c240d04a32a
Nikolay Matrosov <nikthespirit@yandex-team.ru>
2023-01-22 14:58:06 +0100

    make project adjustments to comply serverless runtime

--- ServerlessContainerDemo/Program.cs
+++ ServerlessContainerDemo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseKestrel(
+    serverOptions =>
+    {
+        serverOptions.ListenAnyIP(int.Parse(port));
+    });
 
 var app = builder.Build();
 
@@ -16,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
